ae94054e3d1fa9f477b3963ff6bec0d22923cb29
alex <cingular.alex@gmail.com>
2012-06-04 16:52:40 -0500

    debugging the finite_acqusition with start time

--- gr-uhd/lib/gr_uhd_usrp_source.cc
+++ gr-uhd/lib/gr_uhd_usrp_source.cc
@@ -495,6 +495,7 @@ public:
         if (_start_time_set){
             _start_time_set = false; //cleared for next run
             cmd.time_spec = _start_time;
+            printf(" with start time set \n");
         }
         else{
             cmd.time_spec = get_time_now() + uhd::time_spec_t(reasonable_delay);
