659c39cb2481e1a900107642bbfdbbecccf621e0
alex <cingular.alex@gmail.com>
2012-06-04 18:00:35 -0500

    typo

--- gr-uhd/lib/gr_uhd_usrp_source.cc
+++ gr-uhd/lib/gr_uhd_usrp_source.cc
@@ -499,7 +499,7 @@ public:
         if (_start_time_set){
             _start_time_set = false; //cleared for next run
             cmd.time_spec = _start_time;
-            timeout = cmd.time_spec.get_real_secs() - get_time_now().get.real_secs() + 1.0;
+            timeout = cmd.time_spec.get_real_secs() - get_time_now().get_real_secs() + 1.0;
             fprintf(stderr, "start time set");
         }
         else{
