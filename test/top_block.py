371bdb3b500fce39bc1bd3041065917020007f7c
Alex Zhang <cingular.alex@gmail.com>
2012-06-05 15:41:44 -0500

    June 4 2012 by Alex

---
+++ test/top_block.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+##################################################
+# Gnuradio Python Flow Graph
+# Title: Top Block
+# Generated: Mon Jun  4 16:30:55 2012
+##################################################
+
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio import uhd
+from gnuradio.eng_option import eng_option
+from gnuradio.gr import firdes
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import wx
+
+class top_block(grc_wxgui.top_block_gui):
+
+	def __init__(self):
+		grc_wxgui.top_block_gui.__init__(self, title="Top Block")
+		_icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+		self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
+
+		##################################################
+		# Variables
+		##################################################
+		self.samp_rate = samp_rate = 3200000
+
+		##################################################
+		# Blocks
+		##################################################
+		self.uhd_usrp_source_0 = uhd.usrp_source(
+			device_addr="",
+			stream_args=uhd.stream_args(
+				cpu_format="fc32",
+				channels=range(1),
+			),
+		)
+		self.uhd_usrp_source_0.set_samp_rate(samp_rate)
+		self.uhd_usrp_source_0.set_center_freq(908000000, 0)
+		self.uhd_usrp_source_0.set_gain(0, 0)
+		self.gr_file_sink_0 = gr.file_sink(gr.sizeof_gr_complex*1, "/home/alexzh/gnuradio/test/stream")
+		self.gr_file_sink_0.set_unbuffered(True)
+
+		##################################################
+		# Connections
+		##################################################
+		self.connect((self.uhd_usrp_source_0, 0), (self.gr_file_sink_0, 0))
+
+	def get_samp_rate(self):
+		return self.samp_rate
+
+	def set_samp_rate(self, samp_rate):
+		self.samp_rate = samp_rate
+		self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
+
+if __name__ == '__main__':
+	parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+	(options, args) = parser.parse_args()
+	tb = top_block()
+	tb.Run(True)
+
