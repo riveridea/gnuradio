eb372dae3b047f4038a7129ac9f904ed8f47ea28
Alex Zhang <cingular.alex@gmail.com>
2012-09-12 00:45:09 -0500

    debug

--- gr-digital/examples/narrowband/benchmark_rx.py
+++ gr-digital/examples/narrowband/benchmark_rx.py
@@ -75,8 +75,8 @@ class my_top_block(gr.top_block):
         self.connect(self.source, self.rxpath)
         self.connect(self.source, gr.file_sink(gr.sizeof_gr_complex, "benchmark_sensing.dat"))
         
-        #self.source.u.set_start_on_demand()
-        self.source.u.stop()
+        self.source.u.set_start_on_demand()
+        #self.source.u.stop()
         self.timer = threading.Timer(5, self.start_streaming)
         self.timer.start()
 
