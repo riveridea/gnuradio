271fa288f79283eedcc0b0212ee95f221b49154c
Alex Zhang <cingular.alex@gmail.com>
2012-09-24 11:56:51 -0500

    typo

--- gr-digital/examples/narrowband/tunnel_w_sensor.py
+++ gr-digital/examples/narrowband/tunnel_w_sensor.py
@@ -732,7 +732,7 @@ class cs_mac(object):
         while 1:
             #payload1 = os.read(self.tun_fd, 10*1024)
 
-            payload = output_q.get(TRUE, NONE)
+            payload = output_q.get()
             #print 'pop a packet from the outq'
 
             self.csm.pktno += 1
