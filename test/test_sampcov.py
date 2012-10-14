8df885d0e21e17792c7c0ededfad9283b589c918
Alex Zhang <cingular.alex@gmail.com>
2012-10-14 18:29:29 -0500

    tabs

--- test/test_sampcov.py
+++ test/test_sampcov.py
@@ -17,23 +17,23 @@ import wx
 class my_top_block(gr.top_block):
 
 	def __init__(self):
-        gr.top_block.__init__(self)
+		gr.top_block.__init__(self)
 
 		##################################################
 		# Blocks
 		##################################################
 		self.gr_file_source_0 = gr.file_source(gr.sizeof_gr_complex*1, "/home/alexzh/Dropbox/Public/temp.dat", False)
-        self.s2v = gr.stream_to_vector(gr.sizeof_gr_complex, 32)
-        self.sampcov = digital.digital_swig.sampcov_matrix_calculator(32, 400)
+		self.s2v = gr.stream_to_vector(gr.sizeof_gr_complex, 32)
+		self.sampcov = digital.digital_swig.sampcov_matrix_calculator(32, 400)
 
 
 		##################################################
 		# Connections
 		##################################################
-        self.connect((self.gr_file_source_0, 0), self.s2v)
-        self.connect(self.s2v, self.sampcov)
-        self.connect((self.sampcov, 0), gr.file_sink(gr.sizeof_gr_complex*32*32, "/home/alexzh/Dropbox/Public/sampcov.dat"))
-        self.connect((self.sampcov, 1), gr.file_sink(gr.sizeof_char*32*32, "/home/alexzh/Dropbox/Public/sampcovind.dat"))
+		self.connect((self.gr_file_source_0, 0), self.s2v)
+		self.connect(self.s2v, self.sampcov)
+		self.connect((self.sampcov, 0), gr.file_sink(gr.sizeof_gr_complex*32*32, "/home/alexzh/Dropbox/Public/sampcov.dat"))
+		self.connect((self.sampcov, 1), gr.file_sink(gr.sizeof_char*32*32, "/home/alexzh/Dropbox/Public/sampcovind.dat"))
 
 if __name__ == '__main__':
 	parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
