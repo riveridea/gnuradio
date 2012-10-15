bb7ec151d3a0dbf01027700522d7277e3cae5604
Alex Zhang <cingular.alex@gmail.com>
2012-10-15 16:01:04 -0500

    test

--- test/test_sampcov2.py
+++ test/test_sampcov2.py
@@ -24,25 +24,27 @@ class top_block(grc_wxgui.top_block_gui):
 		##################################################
 		# Blocks
 		##################################################
-		self.pow_cc_0 = baz.pow_cc(2.0, 0.0)
+		#self.pow_cc_0 = baz.pow_cc(2.0, 0.0)
 		self.gr_file_source_0 = gr.file_source(gr.sizeof_gr_complex*1, "/home/alexzh/Dropbox/Public/temp.dat", False)
-		self.gr_file_sink_2 = gr.file_sink(gr.sizeof_gr_complex*1, "test.dat")
-		self.gr_file_sink_2.set_unbuffered(False)
+		self.gr_file_sink1 = gr.file_sink(gr.sizeof_gr_complex*32*32, "/home/alexzh/Dropbox/Public/sampcov.dat")
+ 		self.gr_file_sink2 = gr.file_sink(gr.sizeof_char*32*32, "/home/alexzh/Dropbox/Public/sampcovind.dat")
+		#self.gr_file_sink_2 = gr.file_sink(gr.sizeof_gr_complex*1, "test.dat")
+		#self.gr_file_sink_2.set_unbuffered(False)
 
 		self.s2v = gr.stream_to_vector(gr.sizeof_gr_complex, 32)
-		self.sampcov = digital.digital_swig.sampcov_matrix_calculator(32, 400)        
+		self.sampcov = digital.digital_swig.sampcov_matrix_calculator(32, 100)        
 		##################################################
 		# Connections
 		##################################################
-		self.connect((self.pow_cc_0, 0), (self.gr_file_sink_2, 0))
-		self.connect((self.gr_file_source_0, 0), (self.pow_cc_0, 0))
+		#self.connect((self.pow_cc_0, 0), (self.gr_file_sink_2, 0))
+		#self.connect((self.gr_file_source_0, 0), (self.pow_cc_0, 0))
 
 	        self.connect((self.gr_file_source_0, 0), self.s2v)
 		self.connect(self.s2v, self.sampcov)
-        self.connect(self.gr_file_source_0, gr.file_sink(gr.sizeof_gr_complex, "origin.dat"))
-        self.connnect(self.s2v, gr.file_sink(gr.sizeof_gr_complex*32, "vector.dat"))
-		self.connect((self.sampcov, 0), gr.file_sink(gr.sizeof_gr_complex*32*32, "/home/alexzh/Dropbox/Public/sampcov.dat"))
-		self.connect((self.sampcov, 1), gr.file_sink(gr.sizeof_char*32*32, "/home/alexzh/Dropbox/Public/sampcovind.dat"))        
+	        #self.connect(self.gr_file_source_0, gr.file_sink(gr.sizeof_gr_complex, "origin.dat"))
+        	#self.connect(self.s2v, gr.file_sink(gr.sizeof_gr_complex*32, "vector.dat"))
+		self.connect((self.sampcov, 0), self.gr_file_sink1)
+		self.connect((self.sampcov, 1), self.gr_file_sink2)        
 
 if __name__ == '__main__':
 	parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
