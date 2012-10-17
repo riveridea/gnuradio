769d7746434758f99134967053872b0bfa0b3810
Alex Zhang <cingular.alex@gmail.com>
2012-10-16 23:06:04 -0500

    debug

--- gnuradio-core/src/lib/runtime/gr_block_executor.cc
+++ gnuradio-core/src/lib/runtime/gr_block_executor.cc
@@ -300,7 +300,7 @@ gr_block_executor::run_one_iteration()
       goto were_done;
 
     if (noutput_items == 0){		// we're output blocked
-      printf("BLKD_OUT occurs at %s \n", m->name());
+      std::cout << "!!!!!*****BLKD_OUT occurs at gr block " << m->name() << endl;
       LOG(*d_log << "  BLKD_OUT\n");
       return BLKD_OUT;
     }
