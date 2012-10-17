a813100d82a85b6573efc073c4ed883529b83de0
Alex Zhang <cingular.alex@gmail.com>
2012-10-17 12:06:23 -0500

    debug

--- gr-digital/lib/digital_sampcov_matrix_generator.cc
+++ gr-digital/lib/digital_sampcov_matrix_generator.cc
@@ -117,7 +117,7 @@ digital_sampcov_matrix_generator::general_work (int noutput_items,
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t2);
   diff_s = difftime(t2.tv_sec, t1.tv_sec);
   diff_ns = t2.tv_nsec - t1.tv_nsec;
-  printf ("It took me %f seconds and %f nanoseconds.\n", diff_s, diff_ns);  
+  //printf ("It took me %f seconds and %f nanoseconds.\n", diff_s, diff_ns);  
   
   return 1;
 }
