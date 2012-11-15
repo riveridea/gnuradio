d12b3c2126708c6eb3c61abff9c0c9835f9fce30
Alex Zhang <cingular.alex@gmail.com>
2012-11-15 15:34:56 -0600

    typo

--- gr-digital/examples/narrowband/tdma_data_collect.py
+++ gr-digital/examples/narrowband/tdma_data_collect.py
@@ -164,9 +164,9 @@ class my_top_block(gr.top_block):
                 sys.exit("no connected devices")
             elif (n_devices >= 1):
                 for i in range(n_devices):
-                addr_t = devices[i].to_string()  #ex. 'type=usrp2,addr=192.168.10.109,name=,serial=E6R14U3UP'
-                addrs.append(addr_t[11:30]) # suppose the addr is 192.168.10.xxx
-                addrs[i]
+                        addr_t = devices[i].to_string()  #ex. 'type=usrp2,addr=192.168.10.109,name=,serial=E6R14U3UP'
+                        addrs.append(addr_t[11:30]) # suppose the addr is 192.168.10.xxx
+                        addrs[i]
                 
             if (n_devices == 1 and self._node_type == CLUSTER_NODE):
                 sys.exit("only one devices for the node, we need both communicator and sensor for cluster node")
