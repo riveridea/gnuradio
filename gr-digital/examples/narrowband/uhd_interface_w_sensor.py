b2ab4d5edb776f519b57314334f196aa0ca17b16
riveridea <cingular.alex@gmail.com>
2013-02-05 17:20:04 -0500

    debug the empty queue

--- gr-digital/examples/narrowband/uhd_interface_w_sensor.py
+++ gr-digital/examples/narrowband/uhd_interface_w_sensor.py
@@ -152,7 +152,7 @@ class uhd_interface:
     def update_time_diff(self, usrp):
         msg = self.generate_time_diff(usrp)
         #while (self.tdiff_register.delete_head_nowait() ):
-        while (!self.tdiff_register.empty_p()):
+        while (not self.tdiff_register.empty_p()):
             self.tdiff_register.delete_head_nowait()
             continue
         self.tdiff_register.insert_tail(msg)
