5c3ac08baf7244a781919c27e51a58f81efc5811
alex zhang <cingluar.alex@gmail.com>
2013-10-29 17:20:24 -0500

    raw payload

--- gr-digital/python/packet_utils.py
+++ gr-digital/python/packet_utils.py
@@ -141,8 +141,7 @@ def make_packet(payload, samples_per_symbol, bits_per_symbol,
     else:
         #pkt = ''.join((packed_preamble, packed_access_code, make_header(L, whitener_offset),
         #               (payload_with_crc), '\x55'))
-        pkt = ''.join((make_header(L, whitener_offset),
-                       (payload_with_crc), '\x55'))
+        pkt = ''.join(payload)
 
     if pad_for_usrp:
         pkt = pkt + (_npadding_bytes(len(pkt), int(samples_per_symbol), bits_per_symbol) * '\x55')
